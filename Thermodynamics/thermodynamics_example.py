a1ba5619efc66c1a3c2ce4dbaf01b1ca9e737aba
Amvro23 <91277572+amvro23@users.noreply.github.com>
2022-12-11 18:39:03 +0200

    Commit

--- Thermodynamics/thermodynamics_example.py
+++ Thermodynamics/thermodynamics_example.py
@@ -1,7 +1,7 @@
 import thermodynamics
 import numpy as np
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # INPUT DATA
 # coefficients of shomate equation - nist data from https://webbook.nist.gov/
 co2 = [24.99735, 55.18696, -33.69137, 7.948387, -0.136638, -403.6075, 228.2431, -393.5224]
@@ -11,7 +11,7 @@ h2 = [33.066178, -11.363417, 11.432816, -2.772874, -0.158558, -9.980797, 172.707
 co = [25.56759, 6.096130, 4.054656,  -2.671301,  0.131021, -118.0089, 227.3665, -110.5271]
 ar = [20.78600, 2.825911e-07, -1.464191e-07,  1.092131e-08, -3.661371e-08, -6.197350, 179.9990, 0.00000]
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # enthalpy of formation in kJ/mol
 HF298_ch4 = -74.85
 HF298_co2 = -393.51
@@ -20,7 +20,7 @@ HF298_h2 = 0.0
 HF298_co = -110.53
 HF298_ar = 0.0
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # entropy of formation in J/mol/K
 SF298_ch4 = 186.25
 SF298_co2 = 197.66
@@ -29,7 +29,7 @@ SF298_h2 = 130.68
 SF298_co = 213.79
 SF298_ar = 154.84
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # molar masses of the species containing in the mixture in kg/mol
 Mr_ch4 = 16.04e-03
 Mr_co2 = 44.01e-03
@@ -43,7 +43,7 @@ Mr_ar = 39.948e-03
 T = 873.15 # K
 P = 1 # bar
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # CO2 + CH4 ⇌ 2CO + 2H2 - DRM reaction
 coeffs_DRM = np.array([co2, ch4, co, h2])
 
@@ -64,7 +64,7 @@ print("DRM entropy at {} K is {}".format(T, thermodynamics.entropy_rxn_kj_mol(re
 print("DRM gibbs energy at {} K is {}".format(T, thermodynamics.gibbs_rxn_kj_mol(reaction_DRM, T)[0]), "kJ/mol")
 print("DRM Keq at {} K is {}".format(T, thermodynamics.equilibrium_constant(reaction_DRM, T)[0]))
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # CH4 + H2O ⇌ CO + 3H2 - SRM1 reaction
 coeffs_SRM1 = np.array([ch4, h2o, co, h2])
 
@@ -85,7 +85,7 @@ print("SRM1 entropy at {} K is {}".format(T, thermodynamics.entropy_rxn_kj_mol(r
 print("SRM1 gibbs energy at {} K is {}".format(T, thermodynamics.gibbs_rxn_kj_mol(reaction_SRM1, T)[0]), "kJ/mol")
 print("SRM1 Keq at {} K is {}".format(T, thermodynamics.equilibrium_constant(reaction_SRM1, T)[0]))
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # CH4 + 2H2O ⇌ CO2 + 4H2 - SRM2 reaction
 coeffs_SRM2 = np.array([ch4, h2o, co2, h2])
 
@@ -106,7 +106,7 @@ print("SRM2 entropy at {} K is {}".format(T, thermodynamics.entropy_rxn_kj_mol(r
 print("SRM2 gibbs energy at {} K is {}".format(T, thermodynamics.gibbs_rxn_kj_mol(reaction_SRM2, T)[0]), "kJ/mol")
 print("SRM2 Keq at {} K is {}".format(T, thermodynamics.equilibrium_constant(reaction_SRM2, T)[0]))
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # CO + H2O ⇌ CO2 + H2 - WGS reaction
 coeffs_WGS = np.array([co, h2o, co2, h2])
 
@@ -127,7 +127,7 @@ print("WGS entropy at {} K is {}".format(T, thermodynamics.entropy_rxn_kj_mol(re
 print("WGS gibbs energy at {} K is {}".format(T, thermodynamics.gibbs_rxn_kj_mol(reaction_WGS, T)[0]), "kJ/mol")
 print("WGS Keq at {} K is {}".format(T, thermodynamics.equilibrium_constant(reaction_WGS, T)[0]))
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # determine species in the mixture
 coeffs_species = np.array([ch4, co2, h2o, h2, co, ar])
 
@@ -140,7 +140,7 @@ Mr_species = np.array([Mr_ch4, Mr_co2, Mr_h2o, Mr_h2, Mr_co, Mr_ar])
 # molar ratio of the species in the mixture dimensionless 50% biogas - 50% argon
 y_species = np.array([0.3, 0.2, 0.0, 0.0, 0.0, 0.5])
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # create an object with mixture parameters
 species_enthalpy = thermodynamics.MIXTURE(coeffs_species, HF298_species, y_species, Mr_species)
 
@@ -149,7 +149,7 @@ species_enthalpy.print_params()
 print("partial enthalpies of species in the mixture at {} K is \n{}"
 .format(T, thermodynamics.partial_enthalpies_kj_mol(species_enthalpy, 873.15)[0]), "\n in kJ/mol")
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
 # create an object with mixture parameters
 species_Cp = thermodynamics.MIXTURE(coeffs_species[:, :-3], HF298_species, y_species, Mr_species)
 
@@ -170,4 +170,4 @@ print("density of the mixture at {} K and {} bar is \n{}"
 print("molar mass of the mixture at {} K and {} bar is \n{}"
       .format(T, P, thermodynamics.mr_mix_kg_mol(species_Cp)[0]), "in kg/mol ")
 
-######################################################################################################################
+#---------------------------------------------------------------------------------------------------------------------
